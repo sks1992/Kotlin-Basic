656745100d6ff82fd67276f6d9d34712ea94c7dc
920132 <sandeep.chetram@almarai.com>
2024-09-17 12:01:36 +0530

    first program

---
+++ src/ch_3_first_code/ch_3_first_code.kt
@@ -0,0 +1,15 @@
+package ch_3_first_code
+/**
+Write a code to print hello world
+*/
+//fun use to create a function
+// main() is our entry function
+fun main(){
+    //println -> to go next line
+    println("Hi World!")
+    //print -> print in same line
+    print("Hi ")
+    // to go next line we can also use "\n"
+    print("Sandeep\n")
+    print("Hi Mahesh")
+}
