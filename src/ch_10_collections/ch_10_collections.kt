9caecd2ecb31f2fec125c320a4d2d0387d09070c
920132 <sandeep.chetram@almarai.com>
2024-09-30 16:52:23 +0530

    ch 10 Collections

---
+++ src/ch_10_collections/ch_10_collections.kt
@@ -0,0 +1,19 @@
+package ch_10_collections
+
+/*
+* Collection => A Collection is a way to group element together.
+*
+* What is a list?
+* a list is simply an ordered collection.So its a collection that has order of its elements.
+*
+*
+* What is sets?
+* a Set is a group of unique elements.
+* Order has no significance
+*
+* What is maps
+* Maps is a set of key-value pair.
+*
+* What is iterator
+* An iterator is an Object that loops through element of collection.
+* */
